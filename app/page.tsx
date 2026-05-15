34082428160a57ae4f1deb3aed0f1d5c67fc3349
uzen.lune <uzen.lune@gmail.com>
2026-05-15 00:55:16 +0000

    firebase, 이미지경로 github repository_name 경로에 맞게 수정

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
