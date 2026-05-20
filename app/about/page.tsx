43d1049af364ca9151e73b3ce34a621c7040da19
문연희 <lune@uzen.net>
2026-05-20 16:43:13 +0900

    google Antigravity 커밋 테스트

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,9 +7,9 @@ export default function AboutPage() {
         <h1 className="text-3xl font-semibold mb-4">소개 페이지</h1>
         <p className="text-gray-700 mb-6">이 프로젝트는 Next.js와 Tailwind CSS를 사용하여 만든 React 앱입니다.</p>
         <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            홈으로 돌아가기! test
+          홈으로 돌아가기
         </Link>
-        </main>
+      </main>
     </div>
   )
 }
